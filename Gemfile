2c191232665c727347464dbdb9b399ccc982ea78
zeroeth <zeroeth@pixelflow.org>
2019-07-19 10:44:13 -0700

    Switch to rainbow color library. Print out mission states

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'pry'
-gem 'colored'
+gem 'rainbow'
+gem 'awesome_print'
--- leeter.rb
+++ leeter.rb
@@ -4,16 +4,25 @@ require 'bundler/setup'
 require 'pry'
 require 'json'
 require 'time'
+require 'awesome_print'
+
+require 'rainbow'
+require 'rainbow/refinement'
+using Rainbow
+
+
 
 module Leeter
   class ReadLogFile
     attr_accessor :log_entries
     attr_accessor :event_blacklist
     attr_accessor :correlation_matrix
+    attr_accessor :mission_logs
 
     def initialize
       self.log_entries = []
-      self.event_blacklist = ["Music"]
+      self.event_blacklist = ['Music']
+      self.mission_logs = MissionLogs.new
     end
 
 
@@ -25,7 +34,7 @@ module Leeter
         self.log_entries.push *file_logs
       end
 
-      self.log_entries.sort_by{|entry| entry['timestamp']}
+      self.log_entries.sort_by! { |entry| entry['timestamp'] }
     end
 
 
@@ -51,20 +60,132 @@ module Leeter
 
     def print_log_by_event_count
 
-      log_groups = self.log_entries.group_by{ |entry| entry['event'] }
+      log_groups = self.log_entries.group_by { |entry| entry['event'] }
 
       event_sizes = log_groups.collect do |event, logs_for_event|
         {event: event, log_count: logs_for_event.length}
       end
 
-      event_sizes.sort_by{|event| event[:log_count]}.each do |event|
-        puts "#{event[:event]}: #{event[:log_count]}"
+      event_sizes.sort_by { |event| event[:log_count] }.each do |event|
+        puts "%6d %s" % [event[:log_count], event[:event]]
       end
     end
 
 
     def print_mission_status
+      event_whitelist = ['MissionAbandoned', 'MissionAccepted', 'MissionCompleted', 'MissionFailed', 'MissionRedirected']
+      mission_events = self.log_entries.select{ |entry| event_whitelist.include? entry['event'] }
+
+      # do something with bulk mission data? Does it contain missed state values
+
+      mission_events.each do |mission_event|
+        mission = self.mission_logs.find_mission mission_event['MissionID']
+        mission = mission || Mission.new
+
+        mission.parse_event mission_event
+
+        self.mission_logs.save_mission mission
+      end
+
+
+      self.mission_logs.logs.each do |mission_id, mission|
+        # temporary skip for those without earlier states
+        unless mission.name == ""
+          puts "%s%s" % [mission.present_name, mission.present_states]
+        end
+      end
     end
+  end
+end
+
+
+
+# MISSION CLASSES #######################################################
+
+class Mission
+  attr_accessor :mission_id
+  attr_accessor :name
+  attr_accessor :history
+
+  def initialize
+    self.name = ""
+    self.history = {}
+  end
+
+  def colors
+    {
+      "Name"              => { fg: :black, bg: :orange },
+      "MissionCompleted"  => { fg: :black, bg: :green  },
+      "MissionAccepted"   => { fg: :black, bg: :gray   },
+      "MissionRedirected" => { fg: :black, bg: :blue   },
+      "MissionFailed"     => { fg: :black, bg: :red    },
+      "MissionAbandoned"  => { fg: :black, bg: :brown  }
+    }
+  end
+
+
+  # temporary presenter
+  def present_name
+    truncate_length = 30
+    name_string = self.name.ljust(truncate_length)
+    if name_string.length > truncate_length
+      name_string = name_string.slice(0..truncate_length-4) + "..."
+    end
+    name_string = " %s " % name_string
+    name_string = name_string.fg(self.colors['Name'][:fg])
+    name_string = name_string.bg(self.colors['Name'][:bg])
+  end
+
+  def present_states
+
+    self.history.collect do |event_state, event|
+
+      state_string = " %s " % event['event']
+      state_string = state_string.fg(self.colors[event['event']][:fg])
+      state_string = state_string.bg(self.colors[event['event']][:bg])
+
+    end.join("")
+
+    # get last history's value? or just loop them and print.
+  end
+
+  def parse_event event_json
+    lookup_key = "%d %s %s" % [event_json['MissionID'], event_json['event'], event_json['timestamp'].to_s]
+    if self.history[lookup_key]
+      raise "Already have event #{event_json['event']} for mission #{event_json['MissionID']}"
+    else
+      self.history[lookup_key] = event_json
+    end
+
+    case event_json['event']
+    when 'MissionAccepted'
+      self.mission_id = event_json['MissionID']
+      self.name = event_json['LocalisedName']
+    when 'MissionCompleted'
+    when 'MissionAbandoned'
+    when 'MissionFailed'
+    when 'MissionRedirected'
+    else
+      raise "Dont know what event #{event_json['event']} for mission #{event_json['MissionID']}is"
+    end
+  end
+end
+
+
+
+class MissionLogs
+  attr_accessor :logs
+
+  def initialize
+    self.logs = {}
+  end
+
+  def find_mission mission_id
+    return self.logs[mission_id]
+  end
+
+  def save_mission mission
+    self.logs[mission.mission_id] = mission
   end
 end
 
@@ -73,5 +194,5 @@ leeter_reader = Leeter::ReadLogFile.new
 
 
 leeter_reader.read_all_log_files
-leeter_reader.print_log_by_event_count
+#leeter_reader.print_log_by_event_count
 leeter_reader.print_mission_status
